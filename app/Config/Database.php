6e95116aaf28ae9b8b520c2423fb30d589aa6785
daniela <andreaperez20040528@gmail.com>
2025-02-17 16:35:40 -0500

    tesis finalizada

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -42,7 +42,7 @@ class Database extends Config
         'compress' => false,
         'strictOn' => false,
         'failover' => [],
-        'port' => 5432,
+        'port' => 5433,
     ];
 
     //    /**
